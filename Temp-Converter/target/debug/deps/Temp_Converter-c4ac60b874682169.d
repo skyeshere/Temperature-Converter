2698b52c54b91b09320948bb66f3a535c4bcbab8
skyeshere <skyepluh@gmail.com>
2026-09-09 13:24:21 +0100

    init commit

---
+++ Temp-Converter/target/debug/deps/Temp_Converter-c4ac60b874682169.d
@@ -0,0 +1,5 @@
+/home/skye/Rust/Temperature-Converter/Temp-Converter/target/debug/deps/Temp_Converter-c4ac60b874682169.d: src/main.rs
+
+/home/skye/Rust/Temperature-Converter/Temp-Converter/target/debug/deps/libTemp_Converter-c4ac60b874682169.rmeta: src/main.rs
+
+src/main.rs:
---
+++ Temp-Converter/target/debug/deps/Temp_Converter-e3f7431ef6f091a0.d
@@ -0,0 +1,5 @@
+/home/skye/Rust/Temperature-Converter/Temp-Converter/target/debug/deps/Temp_Converter-e3f7431ef6f091a0.d: src/main.rs
+
+/home/skye/Rust/Temperature-Converter/Temp-Converter/target/debug/deps/libTemp_Converter-e3f7431ef6f091a0.rmeta: src/main.rs
+
+src/main.rs:
